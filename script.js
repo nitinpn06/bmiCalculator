1465eeb59b4176a5ced204251ffd0602636b1f97
nitinpn06 <nitinmittalbshr@gmail.com>
2025-01-05 15:27:42 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// const height = parseInt(document.querySelector('#height').value);
+// const weight = document.querySelector('#weight').value;
+// console.log(weight);
+
+
+let form = document.querySelector('form');
+
+ form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+
+    let results = document.querySelector('#results');
+
+    if(height === ' ' || height<0 || isNaN(height)){
+
+        results.innerHTML=`Please give a valid height ${height}`
+
+    }
+    else if(weight === ' ' || weight<0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    }
+    else{
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+
+},false)
